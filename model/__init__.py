48d0d048cbf3df70d3bebee9e88c68fadfca7c80
WZHsia <154293218+WZHsia@users.noreply.github.com>
2023-12-24 16:50:00 +0800

    Delete model directory

--- model/__init__.py
+++
@@ -1,8 +0,0 @@
-from .UCnet import UCNet, UANet, UGNet, UDNet
-from .Unet import UNet
-from .ULite import ULite, ULite_Mixadd
-from .ResUnet import Resnet34_Unet, Resnet34_Unet_Mix
-from .UnetPlusPlus import UnetPlusPlus, UnetPlusPlus_Mix
-from .UNeXt import UNext, UNext_S, UNext_Mix, UNext_S_Mix
-
-
